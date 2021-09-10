04bd1e29b60d62851a40e91869816940e353efa6
bojandamchevski <bojandamcevski@gmail.com>
2021-09-11 00:38:19 +0200

    hw

---
+++ Homework #1 API Бојан Дамчевски/Homework1/Homework1/Controllers/UserController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            return StatusCode(StatusCodes.Status200OK, StaticDb.UserNames);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<string> Get(int index)
+        {
+            try
+            {
+                if (index < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value!");
+                }
+                if (index >= StaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no username with index {index}");
+                }
+                return StatusCode(StatusCodes.Status200OK, StaticDb.UserNames[index]);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body))
+                {
+                    string username = reader.ReadToEnd();
+                    StaticDb.UserNames.Add(username);
+                    return StatusCode(StatusCodes.Status201Created, "Username created");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(Request.Body))
+                {
+                    string requestBody = streamReader.ReadToEnd();
+                    int index = Int32.Parse(requestBody);
+                    if (index < 0)
+                    {
+                        return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value!");
+                    }
+                    if (index >= StaticDb.UserNames.Count)
+                    {
+                        return StatusCode(StatusCodes.Status404NotFound, $"There is no username with index {index}");
+                    }
+                    StaticDb.UserNames.RemoveAt(index);
+                    return StatusCode(StatusCodes.Status204NoContent, "Username deleted");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+    }
+}
---
+++ Homework #1 API Бојан Дамчевски/Homework1/Homework1/StaticDb.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Homework1
+{
+    public static class StaticDb
+    {
+        public static List<string> UserNames = new List<string>()
+        {
+            "john123",
+            "lucy123",
+            "gary123",
+            "marry123",
+            "greg123"
+        };
+    }
+}
