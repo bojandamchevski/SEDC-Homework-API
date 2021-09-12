8b71ad36b07812d02378d3b6373612b96fcfc7b0
bojandamchevski <bojandamcevski@gmail.com>
2021-09-13 00:19:57 +0200

    update

---
+++ Homework #1 API Бојан Дамчевски/ApiHomework1/ApiHomework1/StaticDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiHomework1
+{
+    public static class StaticDb
+    {
+        public static List<string> Usernames = new List<string>()
+        {
+            "john123",
+            "marry123",
+            "philip123",
+            "amelie123",
+            "jack123",
+            "cris123",
+            "anne123"
+        };
+    }
+}
---
+++ Homework #1 API Бојан Дамчевски/ApiHomework1/ApiHomework1/wwwroot/simple.js
@@ -0,0 +1,57 @@
+﻿let getAllBtn = document.getElementById("btn1");
+let getByIdBtn = document.getElementById("btn2");
+let addNoteBtn = document.getElementById("btn3");
+let deleteBtn = document.getElementById("deleteBtn");
+let getByIdInput = document.getElementById("input2");
+let addNoteInput = document.getElementById("input3");
+let deleteInput = document.getElementById("delete");
+
+let port = "53562";
+let getAllNotes = async () => {
+    let url = "http://localhost:" + port + "/api/users";
+
+    let response = await fetch(url);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+};
+
+let getNoteById = async () => {
+    let url = "http://localhost:" + port + "/api/users/" + getByIdInput.value;
+
+    let response = await fetch(url);
+    let data = await response.text();
+    console.log(data);
+};
+
+let addNote = async () => {
+    let url = "http://localhost:" + port + "/api/users";
+    var response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: addNoteInput.value
+    });
+    var data = await response.text();
+    console.log(data);
+}
+
+let deleteNote = async () => {
+    let url = "http://localhost:" + port + "/api/users";
+    var response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'text/plain'
+        },
+        body: deleteInput.value
+    });
+    var data = await response.text();
+    console.log(data);
+    console.log(response);
+}
+
+getAllBtn.addEventListener("click", getAllNotes);
+getByIdBtn.addEventListener("click", getNoteById);
+addNoteBtn.addEventListener("click", addNote);
+deleteBtn.addEventListener("click", deleteNote);
